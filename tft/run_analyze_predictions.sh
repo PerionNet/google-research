efb6c4e46560150456180140ab0b12db79d5c2fe
EC2 Default User <ec2-user@ip-10-231-11-130.eu-west-1.compute.internal>
2020-09-30 15:28:16 +0000

    Add run_*.sh scripts

---
+++ tft/run_analyze_predictions.sh
@@ -0,0 +1,3 @@
+. ~/tft_outputs/venv/bin/activate
+python -m analyze_predictions cg ~/tft_outputs
+
---
+++ tft/run_download_data.sh
@@ -0,0 +1,3 @@
+. ~/tft_outputs/venv/bin/activate
+python -m script_download_data cg ~/tft_outputs no >> ~/logs/download_data.log 2>&1 &
+
